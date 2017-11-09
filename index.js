8d8f96ab04cd5784a8473cfae1b2702b62c48a14
rnons <remotenonsense@gmail.com>
2017-11-09 21:31:44 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const loaderUtils = require("loader-utils");
+
+const mkRegexp = fnName => {
+  const parts = [fnName, "\\(", "([^)]+)", "\\)"];
+  return new RegExp(parts.join("\\s*"));
+};
+
+const transform = objString => {
+  return objString.replace(/'[^']+\.svg'/g, "require($&)");
+};
+
+const loader = function(source, inputSourceMap) {
+  if (this.cacheable) this.cacheable();
+
+  const config = loaderUtils.getOptions(this) || {};
+  const packageName = config.package || "rnons/elm-svg-loader";
+  config.module = config.module || "InlineSvg";
+  config.tagger = config.tagger || "inline";
+
+  const taggerName =
+    "_" +
+    [
+      packageName.replace(/-/g, "_").replace(/\//g, "$"),
+      config.module.replace(/\./g, "_"),
+      config.tagger
+    ].join("$");
+  const escapedTaggerName = taggerName.replace(/\$/g, "\\$");
+
+  const regexp = mkRegexp(escapedTaggerName);
+
+  const matches = source.match(regexp);
+  source = source.replace(
+    regexp,
+    (match, p1) => taggerName + "(" + transform(p1) + ")"
+  );
+  return source;
+};
+
+module.exports = loader;
